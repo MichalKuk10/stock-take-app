6067531656c81f34e0f51231a21a85ce1ee42cc2
michal <kuk.michal3@gmail.com>
2021-02-18 16:09:44 +0100

    add User classes structure

---
+++ src/main/java/com/project/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.project.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RestController
+@RequestMapping("/api/users")
+@CrossOrigin
+public class UserController {
+
+
+}
---
+++ src/main/java/com/project/converter/UserDTOConverter.java
@@ -0,0 +1,4 @@
+package com.project.converter;
+
+public class UserDTOConverter {
+}
