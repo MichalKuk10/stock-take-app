397d651013a441a336d5db000bba4a99eca0aee3
MiloszBryla <bryla.milosz@gmail.com>
2021-03-11 19:42:16 +0100

    fix users fetching

--- src/main/java/com/project/dao/CarRepo.java
+++ src/main/java/com/project/dao/CarRepo.java
@@ -2,10 +2,7 @@ package com.project.dao;
 
 import com.project.model.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface CarRepo extends JpaRepository<Car, Long> {
